aff22688134e0d2c8d489568260dd1d36c7cfc72
meghajadav <meghasaraiya@gmail.com>
2024-06-12 14:56:16 +0530

    commit changes

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ conda activate interviewcreator
 ### clone the github repository
 
 git clone https://github.com/meghajadav/interviewcreator.git
+
+## Folder Structure
+Create the template.py which will create folder structure and files for the project as follows:
+folder src - __init__.py,  helper.py, prompt.py
+another files outside src is setup.py, requirements.txt, .env, app.py
+another folder research.py which contains trials.ipynb for experiments.
+
+### install requirements.txt
+pip install -r requirements.txt
+
+### -e . in requirements.txt tells the python to see for setup.py and install the .py as a local package
---
+++ template.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+file_list=[
+    'src/prompt.py',
+    'src/__init__.py',
+    'src/helper.py',
+    'setup.py',
+    '.env',
+    'research/trials.py',
+    'app.py',
+    'requirements.txt',
+]
+
+for filepath in file_list:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    # print(filedir, filename)
+    # if not os.path.exists(filedir):
+    if filedir!='':
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating directory {filedir} for the {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w' ) as f:
+            pass
+            logging.info(f"creating file {filepath}")
+    else:
+        logging.INFO(f"{filepath} already exists")
