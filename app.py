44edb2f99d060f359b067c06a18459c6959afad1
meghajadav <meghasaraiya@gmail.com>
2024-06-24 13:19:54 +0530

    Commit

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.encoders import jsonable_encoder
 import uvicorn
+import os
+import aiofiles
+import json
+import csv 
+from src.helper import *
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory='static'), name='static')
@@ -13,6 +18,49 @@ templates = Jinja2Templates(directory='templates')
 @app.get("/")
 async def index(request:Request):
     return templates.TemplateResponse('index.html', {"request":request})
+
+@app.post("/upload")
+async def chat(request:Request, pdf_file: bytes=File(), filename: str=Form(...)):
+    base_folder='static/docs/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    pdf_filename=os.path.join(base_folder, filename)
+    async with aiofiles.open(pdf_filename,'wb') as f:
+        await f.write(pdf_file)
+    
+    response_data = jsonable_encoder(json.dumps({"msg":'success',"pdf_filename":pdf_filename}))
+    res= Response(response_data)
+    return res
+
+    # async with aiofiles.open(pdf_filename,'wb') as f:
+        # await f.write(pdf_file)
+    
+    # response_data
+
+def get_csv(file_path):
+    answer_gen_chain, ques_list=llm_pipeline(file_path)
+    base_folder='static/output/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    output_file = base_folder+'QA.csv'
+    with open(output_file, 'w', newline="", encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Question","Answer"])
+
+        for question in ques_list:
+            print("Question:", question)
+            answer = answer_gen_chain.run(question)
+            print('Answer:', answer)
+            print('-------------------------------\n\n')
+            csv_writer.writerrow([question, answer])
+    return output_file
+
+@app.post("/analyze")
+async def chat(request:Request, pdf_filename: str=Form(...)):
+    output_file = get_csv(pdf_filename)
+    response_data = jsonable_encoder(json.dumps({"output_file":output_file}))
+    res = Response(response_data)
+    return res
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host='0.0.0.0',port=8080, reload=True)
